d68dc18affaa8bfb2cde7b6500f7cab5a9ad0a14
microcai <microcaicai@gmail.com>
2014-12-01 18:48:23 +0800

    积极拥抱大兽模式的大腿

---
+++ src/ca_service.cpp
@@ -0,0 +1,182 @@
+﻿#include "logging.hpp"
+#include "ca_service.hpp"
+
+ca_service::ca_service(io_service_pool& ios, unsigned short port, std::string address /*= "0.0.0.0"*/)
+	: m_io_service_pool(ios)
+	, m_io_service(ios.get_io_service())
+	, m_acceptor(m_io_service)
+	, m_timer(m_io_service)
+{
+	boost::asio::ip::tcp::resolver resolver(m_io_service);
+	std::ostringstream port_string;
+	port_string.imbue(std::locale("C"));
+	port_string << port;
+	boost::system::error_code ignore_ec;
+	boost::asio::ip::tcp::resolver::query query(address, port_string.str());
+	boost::asio::ip::tcp::resolver::iterator endpoint_iterator = resolver.resolve(query, ignore_ec);
+
+	if (ignore_ec)
+	{
+		LOG_ERR << "Server bind address, DNS resolve failed: " << ignore_ec.message() << ", address: " << address;
+		return;
+	}
+
+	boost::asio::ip::tcp::endpoint endpoint = *endpoint_iterator;
+	m_acceptor.open(endpoint.protocol(), ignore_ec);
+
+	if (ignore_ec)
+	{
+		LOG_ERR << "Server open protocol failed: " << ignore_ec.message();
+		return;
+	}
+
+	m_acceptor.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true), ignore_ec);
+
+	if (ignore_ec)
+	{
+		LOG_ERR << "Server set option failed: " << ignore_ec.message();
+		return;
+	}
+
+	m_acceptor.bind(endpoint, ignore_ec);
+
+	if (ignore_ec)
+	{
+		LOG_ERR << "Server bind failed: " << ignore_ec.message() << ", address: " << address;
+		return;
+	}
+
+	m_acceptor.listen(boost::asio::socket_base::max_connections, ignore_ec);
+
+	if (ignore_ec)
+	{
+		LOG_ERR << "Server listen failed: " << ignore_ec.message();
+		return;
+	}
+}
+
+ca_service::~ca_service()
+{}
+
+void ca_service::start()
+{
+	start_impl();
+	continue_timer();
+}
+
+void ca_service::start_impl()
+{
+	m_connection = boost::make_shared<connection>(boost::ref(m_io_service_pool.get_io_service()), boost::ref(*this), &m_connection_manager);
+	m_acceptor.async_accept(m_connection->socket(), boost::bind(&ca_service::handle_accept, this, boost::asio::placeholders::error));
+}
+
+void ca_service::stop()
+{
+	boost::system::error_code ignore_ec;
+	m_timer.cancel(ignore_ec);
+	m_acceptor.close();
+	m_connection_manager.stop_all();
+}
+
+void ca_service::handle_accept(const boost::system::error_code& error)
+{
+	if (!m_acceptor.is_open() || error)
+	{
+		if (error)
+			LOG_ERR << "server::handle_accept, error: " << error.message();
+
+		return;
+	}
+
+	m_connection_manager.start(m_connection);
+
+	start_impl();
+}
+
+void ca_service::on_tick(const boost::system::error_code& error)
+{
+	// TODO: 定时要做的事.
+	continue_timer();
+}
+
+bool ca_service::do_message(google::protobuf::Message* msg, connection_ptr conn)
+{
+	const std::string name = msg->GetTypeName();
+	boost::shared_lock<boost::shared_mutex> l(m_message_callback_mtx);
+	message_callback_table::iterator iter = m_message_callbacks.find(name);
+
+	if (iter == m_message_callbacks.end())
+		return false;
+
+	return iter->second(msg, conn, boost::ref(m_connection_manager)); // 或者直接: return m_message_callback[name](msg, conn, boost::ref(m_connection_manager));
+}
+
+void ca_service::do_connection_notify(int type, connection_ptr conn)
+{
+	boost::shared_lock<boost::shared_mutex> l(m_connection_callback_mtx);
+
+	for (const auto& item : m_connection_callbacks)
+		item.second(type, conn, boost::ref(m_connection_manager));
+}
+
+bool ca_service::add_message_process_moudle(const std::string& name, message_callback cb)
+{
+	boost::unique_lock<boost::shared_mutex> l(m_message_callback_mtx);
+
+	if (m_message_callbacks.find(name) != m_message_callbacks.end())
+	{
+		BOOST_ASSERT("module already exist!" && false);
+		return false;
+	}
+
+	m_message_callbacks[name] = cb;
+	return true;
+}
+
+bool ca_service::del_message_process_moudle(const std::string& name)
+{
+	boost::unique_lock<boost::shared_mutex> l(m_message_callback_mtx);
+
+	if (m_message_callbacks.find(name) == m_message_callbacks.end())
+	{
+		BOOST_ASSERT("not found the moudle" && false);
+		return false;
+	}
+
+	m_message_callbacks.erase(name);
+	return true;
+}
+
+bool ca_service::add_connection_process_moudle(const std::string& name, connection_callback cb)
+{
+	boost::unique_lock<boost::shared_mutex> l(m_connection_callback_mtx);
+
+	if (m_connection_callbacks.find(name) != m_connection_callbacks.end())
+	{
+		BOOST_ASSERT("module already exist!" && false);
+		return false;
+	}
+
+	m_connection_callbacks[name] = cb;
+	return true;
+}
+
+bool ca_service::del_connection_process_moudle(const std::string& name)
+{
+	boost::unique_lock<boost::shared_mutex> l(m_connection_callback_mtx);
+
+	if (m_connection_callbacks.find(name) == m_connection_callbacks.end())
+	{
+		BOOST_ASSERT("not found the moudle" && false);
+		return false;
+	}
+
+	m_connection_callbacks.erase(name);
+	return true;
+}
+
+void ca_service::continue_timer()
+{
+	m_timer.expires_from_now(seconds(1));
+	m_timer.async_wait(boost::bind(&ca_service::on_tick, this, boost::asio::placeholders::error));
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 
 #include <iostream>
+#include <functional>
+#include <thread>
 #include <boost/program_options.hpp>
 #include <boost/asio.hpp>
 #include <openssl/pem.h>
@@ -11,8 +13,9 @@
 namespace po = boost::program_options;
 namespace asio = boost::asio;
 
-static void terminator(boost::asio::io_service& ios)
+static void terminator(io_service_pool& ios, ca_service& serv)
 {
+	serv.stop();
 	ios.stop();
 }
 
@@ -39,6 +42,25 @@ int main(int argc, char **argv)
 			std::cout << desc << "\n";
 			return 0;
 		}
+
+		auto num_threads = std::thread::hardware_concurrency();
+
+		io_service_pool io_pool(num_threads);
+
+		ca_service serv(io_pool, 8086);
+
+		// Ctrl+c异步处理退出.
+		boost::asio::signal_set terminator_signal(io_pool.get_io_service());
+		terminator_signal.add(SIGINT);
+		terminator_signal.add(SIGTERM);
+#if defined(SIGQUIT)
+		terminator_signal.add(SIGQUIT);
+#endif // defined(SIGQUIT)
+		terminator_signal.async_wait(std::bind(&terminator, boost::ref(io_pool), boost::ref(serv)));
+
+		serv.start();
+
+		io_pool.run();
 	}
 	catch(const std::exception& e)
 	{
